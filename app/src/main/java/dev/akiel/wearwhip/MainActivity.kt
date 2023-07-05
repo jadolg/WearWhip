21a5ddce46282c0b97452189dcd2cb65a0cfefab
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2023-07-05 14:46:04 +0200

    Add better startup parameters - Enabled by default - Makes screen stay on - Turns sound on on startup

--- app/src/main/java/dev/akiel/wearwhip/MainActivity.kt
+++ app/src/main/java/dev/akiel/wearwhip/MainActivity.kt
@@ -2,13 +2,15 @@ package dev.akiel.wearwhip
 
 import android.app.Activity
 import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Vibrator
 import android.util.Log
+import android.view.WindowManager
+import android.widget.Switch
 import dev.akiel.wearwhip.Shaker.OnShakeListener
 import dev.akiel.wearwhip.databinding.ActivityMainBinding
-import android.os.Vibrator
-import android.widget.Switch
 
 
 class MainActivity : Activity() {
@@ -25,18 +27,43 @@ class MainActivity : Activity() {
         mShaker = Shaker(this)
         val v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         val mp = MediaPlayer.create(this, R.raw.whipcrack);
+        mp.setVolume(100F, 100F)
+
+        // Keep the screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setupAudio()
+
         mShaker.setOnShakeListener(object : OnShakeListener {
             override fun onShake() {
                 Log.i("SHAKE", "shaking")
                 v.vibrate(100)
                 mp.start()
-
             }
         })
-        mShaker.pause()
+        enableDisableShaker()
+    }
+
+    private fun setupAudio() {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+        audioManager.setStreamVolume(
+            AudioManager.STREAM_RING,
+            audioManager.getStreamMaxVolume(AudioManager.STREAM_RING),
+            0
+        )
+        audioManager.setStreamVolume(
+            AudioManager.STREAM_MUSIC,
+            audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+            0
+        )
     }
 
     fun switchClick(view: android.view.View) {
+        enableDisableShaker()
+    }
+
+    private fun enableDisableShaker() {
         if (switch.isChecked) {
             mShaker.resume()
         } else {
